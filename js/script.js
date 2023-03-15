f645ddbb26e7fffbb0fc41fb8cbaa61aef86989b
João Felipe Correia de Oliveira <101203356+JackHeroes@users.noreply.github.com>
2023-03-15 18:37:41 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function imgSlider(image){
+
+    document.querySelector('#img').src = image;
+}
+
+function changeBgColor(color){
+
+    const bg = document.querySelector('body');
+    bg.style.background = color;
+}
